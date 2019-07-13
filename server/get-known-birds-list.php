b9a3161ea1795aa7a3095fce5d1be008a113140b
leikareipa <drilledicecream@gmail.com>
2019-07-13 21:02:02 +0300

    Return server-side known birds list via PHP

---
+++ server/get-known-birds-list.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ *
+ * 2019 Tarpeeksi Hyvae Soft /
+ * Lintulista
+ * 
+ * Returns as a JSON response a list of the birds we recognize. The user will be able to
+ * add observations of any bird on that list, but not of others.
+ * 
+ */
+
+$knownBirdsData = file_get_contents("./assets/metadata/known-birds.json");
+
+if (!$knownBirdsData)
+{
+    exit(failure("Server-side IO failure: Could not read the list of known birds."));
+}
+
+exit(success($knownBirdsData));
+
+function failure($errorMessage = "")
+{
+    echo json_encode(["valid" => false, "message" => $errorMessage]);
+}
+
+function success($data)
+{
+    echo json_encode(["valid" => true, "data" => $data]);
+}
+
+?>
