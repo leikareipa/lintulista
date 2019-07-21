169742da5481faf9da5fc5e9ecd1b71cfc701823
leikareipa <drilledicecream@gmail.com>
2019-07-21 06:28:58 +0300

    bird(): Add the 'order' and 'family' properties

--- client/src/bird.js
+++ client/src/bird.js
@@ -10,11 +10,13 @@ import {panic_if_undefined} from "./assert.js";
 
 export function bird(args = {})
 {
-    panic_if_undefined(args.name, args.thumbnailUrl);
+    panic_if_undefined(args.name, args.thumbnailUrl, args.family, args.order);
 
     const publicInterface = Object.freeze(
     {
         name: args.name,
+        order: args.order,
+        family: args.family,
         thumbnailUrl: args.thumbnailUrl,
     });
     
