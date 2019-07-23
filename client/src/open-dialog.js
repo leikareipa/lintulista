8e8326d6890e2ea3a5f50d753fc7173a2417514e
leikareipa <drilledicecream@gmail.com>
2019-07-23 09:27:26 +0300

    Add generalized function for rendering dialogs
    Instead of having a separate function for each dialog (e.g.
    render_observation_place_prompt() for ObservationPlacePrompt).

---
+++ client/src/open-dialog.js
@@ -0,0 +1,72 @@
+/*
+ * 2019 Tarpeeksi Hyvae Soft
+ * Lintulista
+ * 
+ */
+
+"use strict";
+
+import {panic_if_undefined} from "./assert.js";
+import {animate} from "./animate.js";
+
+// Renders a dialog component into a new <div> container. Closes the dialog and deletes the
+// container when the user accepts or rejects the dialog via onDialogAccept/onDialogReject.
+//
+// The dialog component to be rendered should be provided via dialog. It should be a
+// React component that calls props.onDialogAccept/props.onDialogReject when the dialog is
+// ready to be closed and unrendered.
+//
+// The parameters (props) to be passed to the dialog component are given via args.parameters;
+// which should be an object containing the desired parameters.
+//
+export function open_dialog(dialog, parameters = {})
+{
+    panic_if_undefined(dialog);
+
+    const dialogContainer = document.createElement("div");
+
+    return new Promise(resolve=>
+    {
+        if (!dialogContainer)
+        {
+            panic("Can't find the container to render the observation list into.");
+            return;
+        }
+
+        const dialogElement = React.createElement(dialog,
+        {
+            ...parameters,
+            onDialogAccept: (returnData)=>
+            {
+                close_this_dialog();
+                resolve(returnData);
+            },
+            onDialogReject: ()=>
+            {
+                close_this_dialog();
+                resolve(null);
+            },
+        });
+
+        document.body.appendChild(dialogContainer);
+        ReactDOM.render(dialogElement, dialogContainer);
+    });
+
+    // Removes the dialog from the DOM.
+    function close_this_dialog()
+    {
+        if (dialogContainer.childElementCount)
+        {
+            /// WIP. Temporary animation.
+            animate(dialogContainer.firstChild, "fadeout", ()=>
+            {
+                ReactDOM.unmountComponentAtNode(dialogContainer);
+                dialogContainer.remove();
+            });
+        }
+        else
+        {
+            dialogContainer.remove();
+        }
+    }
+}
