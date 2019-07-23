2612fc1b34c727ae4ee9acdfd37670375bb4a276
leikareipa <drilledicecream@gmail.com>
2019-07-23 22:02:12 +0300

    Adopt Dialog in QueryObservationPlace

--- client/src/react-components/dialogs/QueryObservationPlace.jsx
+++ client/src/react-components/dialogs/QueryObservationPlace.jsx
@@ -8,13 +8,14 @@
 
 import {panic_if_undefined, panic_if_not_type} from "../../assert.js";
 import {BirdThumbnail} from "../misc/BirdThumbnail.js";
-import {GeoTag} from "../../react-components/misc/GeoTag.js";
+import {GeoTag} from "../misc/GeoTag.js";
+import {Dialog} from "./Dialog.js"
 
 // Displays a modal dialog with which the user can input a place string for an observation.
 //
 // The observation for which the place is prompted is to be provided via props.observation.
 //
-// A callback function which receives the place string once the user accepts the dialog
+// A callback function that receives the place string once the user accepts the dialog
 // should be provided via props.onDialogAccept. It will receive one parameter: the place
 // string that the user entered into the dialog. If the user made no modifications to the
 // string, the original string will be returned.
@@ -31,42 +32,37 @@ export function QueryObservationPlace(props = {})
     const mapUrl = GeoTag.map_link_from_string(placeString);
     const maxPlacenameLength = 60;
 
-    return <div className="Dialog QueryObservationPlace">
-               <div className="title">
-                   <i className="fas fa-map-marker-alt"/> Merkitse havainnon sijainti
+    return <Dialog component="QueryObservationPlace"
+                   title="Merkitse havainnon sijainti"
+                   titleIcon="fas fa-map-marker-alt">
+               <BirdThumbnail bird={props.observation.bird}/>
+               <div className="fields">
+                   <div className="bird-name">
+                       {props.observation.bird.species}:
+                   </div>
+                   <input className="place-name"
+                          type="text"
+                          onKeyPress={(event)=>{if (event.key === "Enter") close_dialog(true)}}
+                          onChange={(event)=>setPlaceString(event.target.value)}
+                          value={placeString? placeString : ""}
+                          placeholder="Esim. Lauttasaari, Helsinki"
+                          spellCheck="false"
+                          maxLength={maxPlacenameLength}
+                          autoFocus/>
+                   <div className="map-link">
+                       <a className={!mapUrl? "disabled" : undefined}
+                          href={mapUrl}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          title="Kokeile karttalinkkiä">
+                              <i className="fas fa-map-marker-alt"/>
+                       </a>
+                   </div>
+                   <div className="character-count">
+                       {placeString.length}/{maxPlacenameLength} merkkiä
+                   </div>
                </div>
-               <div className="form">
-                    <BirdThumbnail bird={props.observation.bird}/>
-                    <div className="fields">
-                        <div className="bird-name">
-                            {props.observation.bird.species}:
-                        </div>
-                        <input className="place-name"
-                               type="text"
-                               onKeyPress={(event)=>{if (event.key === "Enter") close_dialog(true)}}
-                               onChange={(event)=>setPlaceString(event.target.value)}
-                               value={placeString? placeString : ""}
-                               placeholder="Esim. Lauttasaari, Helsinki"
-                               spellCheck="false"
-                               maxLength={maxPlacenameLength}
-                               autoFocus/>
-                        <div className="map-link">
-                            <a className={!mapUrl? "disabled" : undefined}
-                               href={mapUrl}
-                               target="_blank"
-                               rel="noopener noreferrer"
-                               title="Kokeile karttalinkkiä">
-                                   <i className="fas fa-map-marker-alt"/>
-                            </a>
-                        </div>
-                        <div className="toolbar">
-                            <div className="character-count">
-                                {placeString.length}/{maxPlacenameLength} merkkiä
-                            </div>
-                        </div>
-                    </div>
-               </div>
-           </div>
+           </Dialog>
 
     function close_dialog(accept = true)
     {
