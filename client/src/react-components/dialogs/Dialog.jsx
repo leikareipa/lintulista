a1f1f366a55bb855dabcae08f778fb0a2107060f
leikareipa <drilledicecream@gmail.com>
2019-07-23 21:55:42 +0300

    Add the Dialog component
    Intended for use as a base visual element for dialogs, providing e.g.
    the dialog's title.

---
+++ client/src/react-components/dialogs/Dialog.jsx
@@ -0,0 +1,61 @@
+/*
+ * 2019 Tarpeeksi Hyvae Soft
+ * Lintulista
+ * 
+ */
+
+"use strict";
+
+import {panic_if_not_type} from "../../assert.js";
+
+// A base for dialog elements. Displays a title bar with an icon text, and embeds the provided
+// child elements into a <div class="form"> container following the title.
+//
+// The name of the component that builds on the Dialog base is given via props.component. For
+// instance, if you're making a dialog component called AskManyQuestions, you would provide the
+// string props.component = "AskManyQuestions". The string will be appended to Dialog's class
+// name list, so that you can style the dialog in CSS using '.Dialog.AskManyQuestions'.
+//
+// The dialog's title text and icon should be provided via props.title and props.titleIcon; of
+// which both should be strings, the latter giving the Font Awesome class names for the desired
+// icon (e.g. props.titleIcon = "fas fa-map-marker-alt" for a map marker icon).
+//
+// To build a dialog called MyDialog using Dialog as a base, you might have the following JSX:
+//
+//     <Dialog component="MyDialog" title="It's my dialog" titleIcon="fas fa-question">
+//         <span className="sub-title">
+//             Hello!
+//         </span>
+//         <div className="contents">
+//             ...
+//         </div>
+//     </Dialog>
+//
+// In this case, the dialog will be rendered with a title element from Dialog, and the elements
+// we provide inside the <Dialog> tags, which React passes automatically to Dialog via props.children.
+//
+// The elements in the MyDialog example, from above, can be styled using CSS with e.g. the
+// '.Dialog.MyDialog > .form > *' selector. The selector's '.Dialog.MyDialog' part derives from
+// the props.component = "MyDialog" string that we passed to Dialog; and the '> .form >' part
+// from the fact that Dialog embeds all child elements (the ones inside the <Dialog></Dialog>
+// tags) inside a <div className="form"> container.
+//
+export function Dialog(props = {})
+{
+    return <div className={`Dialog ${props.component}`}>
+               <div className="title">
+                   <i className={props.titleIcon}/> {props.title}
+               </div>
+               <div className="form">
+                   {props.children}
+               </div>
+           </div>
+}
+
+Dialog.validateProps = function(props)
+{
+    panic_if_not_type("object", props);
+    panic_if_not_type("string", props.componentName, props.titleIcon, props.title);
+
+    return;
+}
