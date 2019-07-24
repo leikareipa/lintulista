42d1664f8b07db0e24d489ea43b2a9bbac148426
leikareipa <drilledicecream@gmail.com>
2019-07-24 10:59:38 +0300

    animate(): Return a promise instead of taking a callback

--- client/src/animate.js
+++ client/src/animate.js
@@ -6,31 +6,40 @@
 
 "use strict";
 
-import {panic_if_not_type, panic} from "./assert.js";
+import {panic_if_not_type, panic, warn} from "./assert.js";
 
 // A rudimentary wrapper for appending short CSS animations to DOM elements. The 'animation'
 // parameter should provide as a string the name of the CSS animation to run (such that the
 // actual CSS class name is `animation-${animation}`). The 'element' parameter gives the DOM
-// element to apply the animation to. An optional callback function can be provided - it will
-// be called when the animation finishes.
-export function animate(element, animation, callback)
+// element to apply the animation to. Returns a promise that resolves when the animation has
+// finished.
+export function animate(element, animation)
 {
     panic_if_not_type("string", animation);
     panic_if_not_type("object", element);
 
-    if (!element)
+    return new Promise(resolve=>
     {
-        panic("Invalid element for animation.");
-    }
+        if (!element)
+        {
+            panic("Invalid element for animation.");
+        }
 
-    const animationClassName = `animation-${animation}`;
+        const animationClassName = `animation-${animation}`;
 
-    element.classList.add(animationClassName);
-    element.addEventListener("animationend", ()=>
-    {
-        element.classList.remove(animationClassName);
-        callback();
+        if (!element.classList.contains(animationClassName))
+        {
+            element.classList.add(animationClassName);
+            element.addEventListener("animationend", ()=>
+            {
+                element.classList.remove(animationClassName);
+                resolve();
+            });
+        }
+        else
+        {
+            warn("Trying to double queue an animation onto an element.");
+            resolve();
+        }
     });
-
-    return;
 }
