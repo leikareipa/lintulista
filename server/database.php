leikareipa/lintulista

commit 2130e34060a4f446d2fa01044bc77378faacd875

Remove ability to set the place of an observation
The place property was a bit of a privacy concern, since the user could
potentially - and unwittingly - reveal private information about them-
selves via it.

Plus I think not having this property in observations helps streamline
and focus usability.

server/database.php
@@ -116,9 +116,6 @@ class DatabaseAccess
     // Returns all observations associated with the given list. If the given key doesn't exist
     // in the system, a random list of nonce observations will be returned.
     //
-    // Note that the 'place' property of an observation will be returned only for an edit key,
-    // and will be omitted in the retun when accessing with a view key.
-    //
     function get_observations_in_list(string $listKey): array
     {
         // Generate random nonce observations.
@@ -152,15 +149,14 @@ class DatabaseAccess
             $isEditKey = $this->is_edit_key($listKey);
             $listId = $this->get_list_id_of_key($listKey, false);
 
-            $observations = $this->database_query("SELECT species, place, `timestamp` FROM lintulista_observations WHERE list_id = ?",
+            $observations = $this->database_query("SELECT species, `timestamp` FROM lintulista_observations WHERE list_id = ?",
                                                   [$listId]);
 
             $returnObservations = [];
             foreach ($observations as $obs)
             {
                 $returnObservations[] = ["species"=>$obs["species"],
-                                         "timestamp"=>$obs["timestamp"],
-                                         "place"=>($isEditKey? $obs["place"] : "")];
+                                         "timestamp"=>$obs["timestamp"]];
             }
 
             return $returnObservations;
@@ -236,9 +232,6 @@ class DatabaseAccess
         $timestamp = isset($observation["timestamp"])? $observation["timestamp"]
                                                      : exit(ReturnObject::failure("The given observation is missing the required 'timestamp' property."));
 
-        $place = isset($observation["place"])? mb_substr($observation["place"], 0, (new BackendLimits())->value_of("maxPlaceNameLength"), "utf-8")
-                                             : "";
-
         if (!(new KnownBirds())->is_known_species($species))
         {
             exit(ReturnObject::failure("Unable to recognize the species '{$species}'."));
@@ -257,11 +250,6 @@ class DatabaseAccess
                 {
                     $combinedValues[] = "`timestamp` = {$timestamp}";
                 }
-
-                if (isset($place))
-                {
-                    $combinedValues[] = "place = '{$place}'";
-                }
             }
 
             if (count($combinedValues))
@@ -272,8 +260,8 @@ class DatabaseAccess
         }
         else
         {
-            $this->database_command("INSERT INTO lintulista_observations (list_id, species, `timestamp`, place) VALUES (?, ?, ?, ?)",
-                                    [$listId, $species, $timestamp, $place]);
+            $this->database_command("INSERT INTO lintulista_observations (list_id, species, `timestamp`) VALUES (?, ?, ?, ?)",
+                                    [$listId, $species, $timestamp]);
         }
 
         return;
@@ -283,7 +271,7 @@ class DatabaseAccess
     // no such observation could be found.
     private function get_observation_of_species(string $listId, string $species): array
     {
-        $result = $this->database_query("SELECT species, place, `timestamp` FROM lintulista_observations WHERE list_id = ? AND species = ?",
+        $result = $this->database_query("SELECT species, `timestamp` FROM lintulista_observations WHERE list_id = ? AND species = ?",
                                         [$listId, $species]);
 
         if (count($result) === 0)

client/src/backend-access.js
@@ -235,7 +235,6 @@ const httpRequests = Object.freeze(
             {
                 bird: knownBirds.find(b=>(b.species === obs.species)),
                 date: new Date(obs.timestamp*1000),
-                place: obs.place,
             }));
         }
         else
@@ -264,7 +263,6 @@ const httpRequests = Object.freeze(
             body: JSON.stringify({
                 species: observation.bird.species,
                 timestamp: observation.unixTimestamp,
-                place: observation.place,
             }),
         });
 

client/src/observation.js
@@ -29,7 +29,6 @@ export function observation(args = {})
     {
         bird: args.bird,
         date: args.date,
-        place: args.place,
         unixTimestamp: Math.round(args.date.getTime() / 1000),
         dateString,
         timeString,

client/src/react-components/bird-search/BirdSearch.jsx
@@ -79,7 +79,6 @@ export function BirdSearch(props = {})
                                      bird={bird}
                                      clickCallback={select_bird}
                                      dateObserved={observation? observation.dateString : null}
-                                     placeObserved={observation? observation.place : null}
                                      allowAddingToList={props.backend.hasEditRights}/>;
         }
     }

client/src/react-components/bird-search/BirdSearchResult.jsx
@@ -3,7 +3,6 @@
 import {panic_if_not_type} from "../../assert.js"
 import {AsyncIconButton} from "../buttons/AsyncIconButton.js";
 import {BirdThumbnail} from "../misc/BirdThumbnail.js";
-import {GeoTag} from "../tags/GeoTag.js";
 
 // An element displaying information about an individual search result.
 export function BirdSearchResult(props = {})
@@ -27,12 +26,8 @@ export function BirdSearchResult(props = {})
                 <div className="card">
                     <div className="bird-name">
                         {props.bird.species}
-                        {props.placeObserved? <GeoTag place={props.placeObserved}/> : <></>}
                     </div>
-                    <div className="date-observed" style={{marginTop: props.placeObserved? "-1px" : ""}}>
-                        {/* Note: We move the date string up by 1 pixel if the GeoTag is shown, to counter
-                            * the GeoTag pushing the line down by that much. It only does so when its font-size
-                            * property is set below 85% or so; which it is, hence this kludge.*/}
+                    <div className="date-observed">
                         {dateObserved}
                     </div>
                 </div>

client/src/react-components/dialogs/QueryObservationPlace.jsx
@@ -1,93 +1,0 @@
-/*
- * 2019 Tarpeeksi Hyvae Soft
- * Lintulista
- * 
- */
-
-"use strict";
-
-import {panic_if_undefined, panic_if_not_type} from "../../assert.js";
-import {BirdThumbnail} from "../misc/BirdThumbnail.js";
-import {GeoTag} from "../tags/GeoTag.js";
-import {Dialog} from "./Dialog.js"
-
-// Displays a modal dialog with which the user can input a place string for an observation.
-//
-// The observation for which the place is prompted is to be provided via props.observation.
-//
-// A callback function that receives the place string once the user accepts the dialog
-// should be provided via props.onDialogAccept. It will receive one parameter: the place
-// string that the user entered into the dialog. If the user made no modifications to the
-// string, the original string will be returned.
-//
-// A callback function called in case the user rejects the dialog should be provided via
-// props.onDialogReject. It will receive no parameters.
-//
-// The dialog will enforce a length limit on the place name string that the user enters.
-// This limit can be set via props.maxPlaceNameLength.
-//
-export function QueryObservationPlace(props = {})
-{
-    QueryObservationPlace.validateProps(props);
-
-    const [placeName, setPlaceName] = React.useState(props.observation.place || "");
-
-    const mapUrl = GeoTag.map_link_from_string(placeName);
-
-    return <Dialog component="QueryObservationPlace"
-                   title="Merkitse havaintopaikka"
-                   enterAccepts={true}
-                   onDialogAccept={accept}
-                   onDialogReject={reject}>
-               <BirdThumbnail bird={props.observation.bird}
-                              useLazyLoading={false}/>
-               <div className="fields">
-                   <div className="bird-name">
-                       {props.observation.bird.species}:
-                   </div>
-                   <input className="place-name"
-                          type="text"
-                          onChange={update_on_input}
-                          defaultValue={placeName}
-                          placeholder="Esim. Lauttasaari, Helsinki"
-                          spellCheck="false"
-                          maxLength={props.maxPlaceNameLength}
-                          autoFocus/>
-                   <div className={`map-link ${mapUrl? "enabled" : "disabled"}`.trim()}>
-                       <a href={mapUrl}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          title="Kokeile karttalinkkiä">
-                              <i className="fas fa-map-marker-alt"/>
-                       </a>
-                   </div>
-                   <div className="character-count">
-                       {props.maxPlaceNameLength - placeName.length} merkki
-                       {((props.maxPlaceNameLength - placeName.length) !== 1)? "ä" : ""} jäljellä
-                   </div>
-               </div>
-           </Dialog>
-
-    function update_on_input(inputEvent)
-    {
-        setPlaceName(inputEvent.target.value);
-    }
-
-    function accept()
-    {
-        props.onDialogAccept(placeName);
-    }
-    
-    function reject()
-    {
-        props.onDialogReject();
-    }
-}
-
-QueryObservationPlace.validateProps = function(props)
-{
-    panic_if_undefined(props, props.maxPlaceNameLength);
-    panic_if_not_type("function", props.onDialogAccept, props.onDialogReject);
-
-    return;
-}

client/src/react-components/observation-list/ObservationInfo.jsx
@@ -8,7 +8,6 @@
 
 import {panic_if_not_type} from "../../assert.js";
 import {animate} from "../../animate.js";
-import {GeoTag} from "../tags/GeoTag.js";
 
 // Renders structured information about an observation.
 //
@@ -37,35 +36,27 @@ export function ObservationInfo(props = {})
     const refs =
     {
         date: React.useRef(),
-        geotag: React.useRef(),
     }
 
     React.useEffect(()=>
     {
-        if (animation)
+        if (animation && refs[animation.refName].current)
         {
-            animate(animation.ref.current, animation.animationName, (animation.callback || (()=>{})));
+            animate(refs[animation.refName].current, animation.animationName, (animation.callback || (()=>{})));
             queueAnimation(null);
         }
     }, [animation]);
 
     props.setAnimationCallbacks(
     {
-        pulseDateElement: (whenDone)=>queueAnimation({ref:refs.date, animationName:"jump", callback:whenDone}),
-        pulseGeoTagElement: (whenDone)=>queueAnimation({ref:refs.geotag, animationName:"jump", callback:whenDone}),
+        pulseDateElement: (whenDone)=>queueAnimation({refName:"date", animationName:"jump", callback:whenDone}),
     });
 
     return <div className="ObservationInfo">
                 <div className="bird-name">
                     {props.observation.bird.species}
-                    <div ref={refs.geotag} style={{display:"inline-block"}}>
-                        <GeoTag place={props.observation.place}/>
-                    </div>
                 </div>
-                <div className="date" ref={refs.date} style={{marginTop: props.observation.place? "-1px" : ""}}>
-                    {/* Note: We move the date string up by 1 pixel if the GeoTag is shown, to counter
-                      * the GeoTag pushing the line down by that much. It only does so when its font-size
-                      * property is set below 85% or so; which it is, hence this kludge.*/}
+                <div className="date" ref={refs.date}>
                     {props.observation.dateString}
                 </div>
            </div>

client/src/react-components/observation-list/ObservationList.jsx
@@ -91,7 +91,7 @@ export function ObservationList(props = {})
     // The element displayed in the observation list when the list is empty (i.e. when
     // there are no observations).
     const emptyElement = <div className="intro animation-jump">
-                             <h3><i className="fas fa-feather-alt"/> Tervetuloa Lintulistaan!</h3>
+                             <h3><i className="fas fa-feather-alt"/> Tervetuloa Lintulistalle!</h3>
                              <p>Löydät sivun käyttöohjeet <a href="/ohjeet/" target="_blank" rel="noopener noreferred">
                                 <i className="fas fa-link fa-sm"/> tästä</a>. Ohjeet sisältävät mm. tärkeää yksityisyystietoa,
                                 ja niiden vilkaiseminen onkin suosisteltua ennen sivuston varsinaista käyttöönottoa.</p>
@@ -228,13 +228,13 @@ export function ObservationList(props = {})
 
     async function save_observation_list_to_csv_file()
     {
-        let csvString = "Päiväys, Laji, Heimo, Lahko, Havaintopaikka\n";
+        let csvString = "Ensihavainto, Laji, Heimo, Lahko\n";
 
         props.backend.observations().forEach(obs=>
         {
             const dateString = new Intl.DateTimeFormat("fi-FI").format(obs.date);
 
-            csvString += `${dateString||""}, ${obs.bird.species||""}, ${obs.bird.family||""}, ${obs.bird.order||""}, ${obs.place||""},\n`;
+            csvString += `${dateString||""}, ${obs.bird.species||""}, ${obs.bird.family||""}, ${obs.bird.order||""},\n`;
         });
 
         saveAs(new Blob([csvString], {type: "text/plain;charset=utf-8"}), "lintulista.csv");
@@ -321,11 +321,11 @@ export function ObservationList(props = {})
             return null;
         }
 
-        const elementIdx = observationElements.map(e=>e.observation.bird.species).findIndex(species=>(species === modifiedObservation.bird.species));
-        observationElements.splice(elementIdx, 1, create_observation_element(modifiedObservation));
-
-        sort_observation_list();
-        redraw_elements_list();
+        if (sortListBy === "date")
+        {
+            sort_observation_list();
+            redraw_elements_list();
+        }
 
         return (props.backend.observations().find(obs=>obs.bird.species === existingObservation.bird.species) || null);
     }
@@ -347,11 +347,11 @@ export function ObservationList(props = {})
             return null;
         }
 
-        const elementIdx = observationElements.map(e=>e.observation.bird.species).findIndex(species=>(species === modifiedObservation.bird.species));
-        observationElements.splice(elementIdx, 1, create_observation_element(modifiedObservation));
-
-        sort_observation_list();
-        redraw_elements_list();
+        if (sortListBy === "place")
+        {
+            sort_observation_list();
+            redraw_elements_list();
+        }
 
         return (props.backend.observations().find(obs=>obs.bird.species === existingObservation.bird.species) || null);
     }

commit b3a9d79ba85cfb1d6cdd05672fde52081e957d66

database.php: Fix wrong SQL parameter count in adding observation
Regressed in 2130e34060a4f446d2fa01044bc77378faacd875.

server/database.php
@@ -260,7 +260,7 @@ class DatabaseAccess
         }
         else
         {
-            $this->database_command("INSERT INTO lintulista_observations (list_id, species, `timestamp`) VALUES (?, ?, ?, ?)",
+            $this->database_command("INSERT INTO lintulista_observations (list_id, species, `timestamp`) VALUES (?, ?, ?)",
                                     [$listId, $species, $timestamp]);
         }
 