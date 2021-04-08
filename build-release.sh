c2390a129b159f6fa3cbd4c9c1bdd6829eaf3078
leikareipa <drilledicecream@gmail.com>
2021-04-08 09:03:16 +0300

    Remove server code
    A new server component (see repo "lintulista-server") is going to
    replace this removed one in the near future.

--- build-release.sh
+++ build-release.sh
@@ -1,4 +1,3 @@
 cp .babelrc-release ./source/client/.babelrc
 ./node_modules/.bin/babel ./source/client/ --out-dir ./distributable/js/
 rm ./source/client/.babelrc
-cp -r ./source/server/ ./distributable/
