d30a762a6955ac79943964e75fe57148d2b22c0b
leikareipa <drilledicecream@gmail.com>
2021-04-12 02:16:23 +0300

    Move Redux store into its own source file

---
+++ source/client/redux-store.js
@@ -0,0 +1,75 @@
+/*
+ * 2021 Tarpeeksi Hyvae Soft
+ * 
+ * Software: Lintulista
+ * 
+ */
+
+"use strict";
+
+const initialState = {
+    isLoggedIn: false,
+    is100LajiaMode: false,
+    observations: [],
+    knownBirds: [],
+};
+
+export const store = Redux.createStore(reducer);
+
+function reducer(state = initialState, action)
+{
+    switch (action.type)
+    {
+        case "set-100-lajia-mode":
+        {
+            return {
+                ...state,
+                is100LajiaMode: action.isEnabled,
+            };
+        }
+        case "set-logged-in":
+        {
+            return {
+                ...state,
+                isLoggedIn: action.isLoggedIn,
+            };
+        }
+        case "set-observations":
+        {
+            return {
+                ...state,
+                observations: action.observations,
+            };
+        }
+        case "set-known-birds":
+        {
+            return {
+                ...state,
+                knownBirds: action.knownBirds,
+            };
+        }
+        case "add-observation":
+        {
+            const newObservationsList = [
+                ...state.observations,
+                action.observation,
+            ];
+
+            return {
+                ...state,
+                observations: newObservationsList,
+            };
+        }
+        case "delete-observation":
+        {
+            const observation = action.observation;
+            const newObservationsList = state.observations.filter(o=>o.species !== observation.species);
+
+            return {
+                ...state,
+                observations: newObservationsList,
+            };
+        }
+        default: return state;
+    }
+}
--- source/client/render/render-app.js
+++ source/client/render/render-app.js
@@ -1,18 +1,18 @@
+/*
+ * 2021 Tarpeeksi Hyvae Soft
+ * 
+ * Software: Lintulista
+ * 
+ */
+
 "use strict";
 
 import {ObservationList} from "../react-components/observation-list/ObservationList.js";
 import {BackendAccess} from "../backend-access.js";
-
-const initialStoreState = {
-    isLoggedIn: false,
-    is100LajiaMode: false,
-    observations: [],
-    knownBirds: [],
-};
+import {store} from "../redux-store.js";
 
 export async function start_app(listKey, container)
 {
-    const store = Redux.createStore(reducer);
     const backend = await BackendAccess(listKey, store);
 
     store.subscribe(()=>console.log(store.getState()));
@@ -26,63 +26,5 @@ export async function start_app(listKey, container)
             />
 
         </ReactRedux.Provider>,
-    container);
-}
-
-function reducer(state = initialStoreState, action)
-{
-    switch (action.type)
-    {
-        case "set-100-lajia-mode":
-        {
-            return {
-                ...state,
-                is100LajiaMode: action.isEnabled,
-            };
-        }
-        case "set-logged-in":
-        {
-            return {
-                ...state,
-                isLoggedIn: action.isLoggedIn,
-            };
-        }
-        case "set-observations":
-        {
-            return {
-                ...state,
-                observations: action.observations,
-            };
-        }
-        case "set-known-birds":
-        {
-            return {
-                ...state,
-                knownBirds: action.knownBirds,
-            };
-        }
-        case "add-observation":
-        {
-            const newObservationsList = [
-                ...state.observations,
-                action.observation,
-            ];
-
-            return {
-                ...state,
-                observations: newObservationsList,
-            };
-        }
-        case "delete-observation":
-        {
-            const observation = action.observation;
-            const newObservationsList = state.observations.filter(o=>o.species !== observation.species);
-
-            return {
-                ...state,
-                observations: newObservationsList,
-            };
-        }
-        default: return state;
-    }
+        container);
 }
