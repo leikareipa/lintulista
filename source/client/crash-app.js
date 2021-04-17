0743b3dd69d9af5a8af825a98823b348e0bba877
leikareipa <drilledicecream@gmail.com>
2021-04-17 21:53:58 +0300

    Add a way to crash the app with a bluescreen error
    For terminal errors that can't be handled in any other way.

---
+++ source/client/crash-app.js
@@ -0,0 +1,55 @@
+/*
+ * 2021 Tarpeeksi Hyvae Soft
+ *
+ * Software: Lintulista
+ * 
+ */
+
+"use strict";
+
+let appHasCrashed = false;
+
+// Fully halts execution of the Lintulista app and displays a bluescreen error.
+export function ll_crash_app(error)
+{
+    // Avoid an infinite loop.
+    if (appHasCrashed) {
+        return;
+    }
+    else {
+        appHasCrashed = true;
+    }
+
+    let errorMessage = "Unknown error";
+
+    if (typeof error === "string") {
+        errorMessage = error;
+    }
+    else if ((typeof error === "object") &&
+             (typeof error.message === "string")) {
+        errorMessage = error.message;
+    }
+    else if ((typeof error === "object") &&
+             (typeof error.reason === "object") &&
+             (typeof error.reason.message === "string")) {
+        errorMessage = error.reason.message
+    }
+
+    const appElement = document.getElementById("lintulista");
+    const bluescreenElement = document.getElementById("blue-screen");
+    const errorMessageElement = bluescreenElement.querySelector(".error-description");
+
+    // Normally we'd assert to make sure these elements exist, but since the
+    // app is already in a crashed state, it wouldn't be able to handle the
+    // assertions.
+    if (appElement &&
+        bluescreenElement &&
+        errorMessageElement)
+    {
+        appElement.remove();
+        errorMessageElement.innerHTML = errorMessage;
+        bluescreenElement.style.display = "flex";
+    }
+
+    return;
+}
